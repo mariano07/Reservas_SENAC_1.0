c70051cb7cc48a0a6efeb977ddc43ec3a9fc84f9
Arthur <84734090+rapach01@users.noreply.github.com>
2023-01-10 19:54:55 -0300

    conexao com o banco

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
+<?php
+    session_start();
+
+    $servername="localhost";
+    $database="reservassenac";
+    $username="root";
+    $password="root";
+
+    $con = mysqli_connect($servername, $username, $password, $database);
+
+    if(!$con){              
+        mysqli_connect_error();
+    }
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
