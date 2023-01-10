b5345ef1119c23323986568c15ab908d1e3cc2a8
Cristopher Machado <cristopherpiussi@gmail.com>
2023-01-10 19:34:10 -0300

    criação index

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
     
-<!-- header -->
-<header>
-
-
-
-</header>
-<!-- close header -->
-
-<!-- main -->
-<main>
-
-
-
-</main>
-<!-- close main -->
-
-<!-- footer -->
-<footer>
-
-
-
-</footer>
-<!-- close footer -->
 </body>
 </html>
