b711b0a453b14256bcdf51eb7b21e79a344fca16
Anthony Pessato da Silva Santos <104658705+Anthonypssantos@users.noreply.github.com>
2023-01-12 19:04:41 -0300

    2 paginas colocadas, 4 .css e 1 imagem

--- reserva.php
+++ reserva.php
@@ -22,5 +22,64 @@
 </head>
 <body>
     
+<div id="booking" class="section">
+		<div class="section-center">
+			<div class="container">
+				<div class="row">
+					<div class="booking-form">
+						<div class="booking-bg"></div>
+						<form action="" method="POST">
+							<div class="form-header">
+								<h2><img src="img/senac_logo.png" alt=""><br> Bem-vindo</h2>
+							</div>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label">data</span>
+										<input class="form-control" type="date" required name="data" id="data">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label">hora</span>
+										<input class="form-control" type="time" required name="hora" id="hora">
+									</div>
+								</div>			
+								<div class="col-md-6">
+									<div class="form-group">
+										<span class="form-label">Salas</span>			
+										<div include="form-input-select()">
+											<select required name="salas" id="salas">
+												<option value="" hidden>Salas de aula</option>
+												<option value="1">Option 1</option>
+												<option value="2">Option 2</option>
+												<option value="3">Option 3</option>
+												<option value="4">Option 4</option>
+												<option value="5">Option 5</option>
+											</select>
+										</div>
+									</div>
+								</div>
+							</div>
+							<span class="form-label">Informe aqui os itens nescessarios na sala</span>
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<textarea style="resize: none" id="story" name="story" rows="5" cols="44">
+										</textarea>
+									</div>
+								</div>
+							</div>
+							<div class="form-btn">
+								<button class="submit-btn ">Agendar</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
 </body>
 </html>
---
+++ sisre.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>sistemareserva</title>
+  <link rel="stylesheet" href="css/stylere.css">
+  <link rel="stylesheet" href="css/button.css">
+</head>
+<body>
+
+<form action="" method="post">
+<div class="container">
+	<table>
+		<thead>		
+			<tr>
+			<img src="img/senac_logo.png" alt="" style="padding-left: 40%;">		
+				<th>Andar</th>
+				<th>Sala</th>
+				<th>Professor</th>
+				<th>Hora</th>
+				<th>Materiais</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Andar 1</td>
+				<td>Sala 1</td>
+				<td>Professor 1</td>
+				<td>Hora 1</td>
+				<td>
+					<select>
+						<option value=""></option>
+						<option value="">Camera: 1</option>
+						<option value="">Computador: 2</option>
+						<option value="Cadeira: ">3</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Andar 2</td>
+				<td>Sala 2</td>
+				<td>Professor 2</td>
+				<td>Hora 2</td>
+				<td>
+					<select>
+						<option value=""></option>
+						<option value="Camera: ">1</option>
+						<option value="Computador: ">2</option>
+						<option value="Cadeira: ">3</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Andar 3</td>
+				<td>Sala 3</td>
+				<td>Professor 3</td>
+				<td>Hora 3</td>
+				<td>
+					<select>
+						<option value=""></option>
+						<option value="Camera: ">1</option>
+						<option value="Computador: ">2</option>
+						<option value="Cadeira: ">3</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Andar 4</td>
+				<td>Sala 4</td>
+				<td>Professor 4</td>
+				<td>Hora 4</td>
+				<td>
+					<select>
+						<option value=""></option>
+						<option value="Camera: ">1</option>
+						<option value="Computador: ">2</option>
+						<option value="Cadeira: ">3</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Andar 5</td>
+				<td>Sala 5</td>
+				<td>Professor 5</td>
+				<td>Hora 5</td>
+				<td>
+					<select>
+						<option value=""></option>
+						<option value="Camera: ">1</option>
+						<option value="Computador: ">2</option>
+						<option value="Cadeira: ">3</option>
+					</select>
+				</td>
+			</tr>	
+		</tbody>
+	</table>
+	<button>proximo</button>
+</div>
+</form>  
+</body>
+</html>
+
+
+</body>
+</html>
