6a05c308050e40295420a9cb2c1afeaac772429b
Anthony Pessato da Silva Santos <104658705+Anthonypssantos@users.noreply.github.com>
2023-01-12 20:57:00 -0300

    dashboard_pedagogico add navbar

--- dashboard_pedagogico.php
+++ dashboard_pedagogico.php
@@ -13,15 +13,6 @@
     // }
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Pedagogico</title>
-</head>
-<body>
-<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -32,11 +23,37 @@
 </head>
 <body>
 
-<main>
+<header>
+<nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="images/senac_logo.png" style="height: 50px; width: 100px;"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Senac-RS</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Perfil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Reservar</a>
+          </li>
+      </div>
+    </div>
+  </div>
+</nav>
+</header>
+
+<main style="padding-top: 83px;">
 <table class="table table-dark table-striped-columns">
 <thead>
     <tr>
-      <th scope="col"><img src="images/senac_logo.png" style="height: 50px; width: 100px;"></th>
+      <th scope="col"></th>
       <th scope="col">Andar</th>
       <th scope="col">Sala</th>
       <th scope="col">Professor</th>
@@ -44,77 +61,12 @@
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row"></th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-	  <td>
-		
-	  </td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-	  <td>b</td>
-    </tr>
 	<tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-	  <td>b</td>
-    </tr>
-	<tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-	  <td>b</td>
-    </tr>
-	<tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-	  <td>b</td>
-    </tr>
-	<tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-	  <td>b</td>
-    </tr>
-	<tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-	  <td>b</td>
-    </tr>
-	<tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-	  <td>b</td>
-    </tr>
-	<tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-	  <td>b</td>
-    </tr>
-	<tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-	  <td>b</td>
+      <th scope="row">ID</th>
+      <td><input type="text" disabled></td>
+      <td><input type="text" disabled></td>
+      <td><input type="text" disabled></td>
+	  <td><button type="button" class="btn btn-dark">Deletar</button></td>
     </tr>
     <tr>
       <th scope="row"></th>
@@ -125,14 +77,11 @@
   </tbody>
 </table>	
 </main>
-
 <footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </footer>
 
-</body>
-</html>
 </body>
 </html>
