aed66cc695ef8c804a4b96b6e56a5eef980294f5
Anthony Pessato da Silva Santos <104658705+Anthonypssantos@users.noreply.github.com>
2023-01-12 21:44:51 -0300

    dashboard update pedagoga e add css pedagoga

--- dashboard_pedagogico.php
+++ dashboard_pedagogico.php
@@ -20,37 +20,32 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/dasboard_pedagoga.css">
 </head>
 <body>
 
 <header>
-<nav class="navbar navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="images/senac_logo.png" style="height: 50px; width: 100px;"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+<nav class="navbar navbar-expand-lg">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="" alt="" height="36">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Senac-RS</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Perfil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Reservar</a>
-          </li>
-      </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+	  <li class="nav-item">
+          <a class="nav-link" href="#">Reservar</a>
+        </li>
+      </ul>
     </div>
   </div>
 </nav>
 </header>
 
-<main style="padding-top: 83px;">
-<table class="table table-dark table-striped-columns">
+<main style="padding-top: 14px;">
+<table class="table table-light table-striped-columns">
 <thead>
     <tr>
       <th scope="col"></th>
@@ -63,10 +58,10 @@
   <tbody>
 	<tr>
       <th scope="row">ID</th>
-      <td><input type="text" disabled></td>
-      <td><input type="text" disabled></td>
-      <td><input type="text" disabled></td>
-	  <td><button type="button" class="btn btn-dark">Deletar</button></td>
+      <th scope="col">Andar</th>
+      <th scope="col">Sala</th>
+      <th scope="col">Professor</th>
+	  <td><button type="button" class="btn btn-dark">Deletar</button></td>  
     </tr>
     <tr>
       <th scope="row"></th>
