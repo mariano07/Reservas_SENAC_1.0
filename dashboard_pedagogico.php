70c49c7a89a5b6e9026d93dc492adc6e1de4e290
Cristopher Machado <cristopherpiussi@gmail.com>
2023-01-14 00:08:48 -0300

    mudanças visuais nos dashboards

--- dashboard_pedagogico.php
+++ dashboard_pedagogico.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard</title>
+    <link href="images/senac_icone.png" type="image/x-icon" rel="shortcut icon">
+    <title>Dashboard Pedagógico</title>
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/dashboard/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
@@ -72,6 +73,7 @@
               <th scope="col">Dia</th>
               <th scope="col">Horário</th>
               <th scope="col">Professor</th>
+              <th scope="col">Status</th>
               <th scope="col">Aprovar</th>
               <th scope="col">Deletar</th>
             </tr>
@@ -83,6 +85,7 @@
               <td>01/01/2023</td>
               <td>13:00</td>
               <td>Mariano</td>
+              <td>Aprovado</td>
               <td><button type="button" class="btn btn-success">Aprovar</button></td> 
 	            <td><button type="button" class="btn btn-danger">Deletar</button></td> 
             </tr>
