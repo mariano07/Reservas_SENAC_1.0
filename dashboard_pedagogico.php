69e2c2788d90bca99814583aa681e67dddcf036b
Cristopher Machado <cristopherpiussi@gmail.com>
2023-01-16 19:56:00 -0300

    correção de textos das salas

--- dashboard_pedagogico.php
+++ dashboard_pedagogico.php
@@ -73,8 +73,6 @@
               <th scope="col">Dia</th>
               <th scope="col">Horário</th>
               <th scope="col">Professor</th>
-              <th scope="col">Status</th>
-              <th scope="col">Aprovar</th>
               <th scope="col">Deletar</th>
             </tr>
           </thead>
@@ -84,9 +82,7 @@
               <td>102</td>
               <td>01/01/2023</td>
               <td>13:00</td>
-              <td>Mariano</td>
-              <td>Aprovado</td>
-              <td><button type="button" class="btn btn-success">Aprovar</button></td> 
+              <td>Mariano</td> 
 	            <td><button type="button" class="btn btn-danger">Deletar</button></td> 
             </tr>
           </tbody>
