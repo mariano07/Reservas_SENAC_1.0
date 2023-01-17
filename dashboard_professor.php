c6f1c7ecd9bea071ea67031d2559a42f96a19255
Arthur <84734090+rapach01@users.noreply.github.com>
2023-01-16 21:18:54 -0300

    nomes arquivos

--- dashboard_professor.php
+++ dashboard_professor.php
@@ -31,20 +31,26 @@
     <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
       <div class="position-sticky pt-3 sidebar-sticky">
         <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="user" class="align-text-bottom"></span>
+              Minhas Reservas
+            </a>
+          </li>
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">
+            <a class="nav-link" aria-current="page" href="#" onclick="verReservas()">
               <span data-feather="calendar" class="align-text-bottom"></span>
               Ver Reservas
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="reservar_sala.php">
               <span data-feather="edit-3" class="align-text-bottom"></span>
               Reservar
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="#" onclick="mudarSenha()">
               <span data-feather="lock" class="align-text-bottom"></span>
               Mudar Senha
             </a>
@@ -92,5 +98,66 @@
     <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="js/dashboard.js"></script>
+    <script>
+      function minhasRservas(){
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Reservas</h1>
+      </div>
+
+      <div class="table-responsive">
+        <table class="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">Andar</th>
+              <th scope="col">Sala</th>
+              <th scope="col">Dia</th>
+              <th scope="col">Horário</th>
+              <th scope="col">Deletar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>102</td>
+              <td>01/01/2023</td>
+              <td>13:00</td>
+              <td><button type="button" class="btn btn-danger">Deletar</button></td> 
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      }
+      function verReservas() {
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Reservas</h1>
+      </div>
+
+      <div class="table-responsive">
+        <table class="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">Andar</th>
+              <th scope="col">Sala</th>
+              <th scope="col">Dia</th>
+              <th scope="col">Horário</th>
+              <th scope="col">Deletar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>102</td>
+              <td>01/01/2023</td>
+              <td>13:00</td>
+              <td><button type="button" class="btn btn-danger">Deletar</button></td> 
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      }
+      function mudarSenha(){
+
+      }
+    </script>
   </body>
 </html>
