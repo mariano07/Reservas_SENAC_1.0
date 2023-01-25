929c5cf79df39b141fcf1baf7123d2106f00a046
Cristopher Machado <cristopherpiussi@gmail.com>
2023-01-24 21:07:16 -0300

    mudança da mensagem de procurar

--- dashboard_admin.php
+++ dashboard_admin.php
@@ -106,7 +106,7 @@ echo '<script>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <input class="form-control form-control-white w-100 rounded-0 border-0" type="text" placeholder="Procurar" aria-label="Search">
+    <input class="form-control form-control-white w-100 rounded-0 border-0" type="text" placeholder="Procure pelo nome de um professor,sala ou andar" aria-label="Search">
     <div class="navbar-nav">
       <div class="nav-item text-nowrap">
         <a class="nav-link px-3" href="#">Procurar</a>
