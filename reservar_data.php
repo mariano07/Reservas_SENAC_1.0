4e29f88e8fd0c3deb0baed35361b11d4406bd116
Cristopher Machado <cristopherpiussi@gmail.com>
2023-02-01 19:06:21 -0300

    mudanças nas paginas de confirmacao

--- reservar_data.php
+++ reservar_data.php
@@ -37,7 +37,7 @@ $_SESSION['sala'] = $_POST['sala'];
 							<div class="form-header" style="margin-left: 175px;">
 								<img style="padding-left: 25%; padding-top: 5%;" src="images/senac_logo.png" style="width: 200px;">
 							</div>
-                            <form action="confirmacao.php" method="post">
+                            <form action="confirmacao_reserva.php" method="post">
 							<div class="row">
 								<div class="col-md-6">
 									<div class="form-group" style="margin-top: 40px;">
